73a454b865e25feb713926e8fbbce7e78e686d6a
Manuel Reyes <manuel@23people.co>
2015-06-18 23:21:11 -0300

    binding to cloud service

--- www/js/app/productsViewModel.js
+++ www/js/app/productsViewModel.js
@@ -2,6 +2,28 @@ var ProductsViewModel = function(parent){
 
     var self = this;
     
+    var self.products = ko.observableArray();
+    
+    self.getProducts = function(){
+    
+        $.ajax({
+
+            url: 	'http://unabrestmobile-mreyesexamples.rhcloud.com/api/products',
+            type: 	'GET',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            success: function(data){
+                console.log(data);
+                self.products(data);
+            },
+            error: function(xhr, type){
+                console.log('error');
+            }
+
+        });
+        
+    };
+    
     self.logout = function() {
         window.plugins.googleplus.logout(
             function (result) {
